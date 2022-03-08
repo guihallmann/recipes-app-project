42729012d490bc96de72443e4cfe807fb13c7c8b
Vitor Campos <vitorcampos742@gmail.com>
2022-03-08 15:41:29 -0300

    requisito 2 a 8 - pagina de login

--- src/services/Rotas.js
+++ src/services/Rotas.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Header from '../components/Header';
+import Login from '../pages/Login';
 
 function Rotas() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/header" component={ Header } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </BrowserRouter>
   );
