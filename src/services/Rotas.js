938c4ba1b5eab7ffe7cf69d29984a14426af1924
Guilherme <guilhermehallmann@gmail.com>
2022-03-08 15:39:59 -0300

    Requisito 9 - Header

--- src/services/Rotas.js
+++ src/services/Rotas.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Header from '../components/Header';
+import Foods from '../pages/Foods';
 
 function Rotas() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/header" component={ Header } />
+        <Route exact path="/foods" component={ Foods } />
       </Switch>
     </BrowserRouter>
   );
