e58715852eeb1fbfaf4ac5b5c300606563cb3d95
Vitor Campos <vitorcampos742@gmail.com>
2022-03-09 18:53:47 -0300

    requisito 25 a 30

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,18 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipesContext from '../context/RecipesContext';
 import Card from '../components/Card';
-import { MAX_LIST_SIZE } from '../data/consts';
+import { MAX_LIST_SIZE, MAX_NUMBER_BUTTON } from '../data/consts';
+import {
+  getTwelveDrinks,
+  getDrinkCategories,
+  searchByDrinkCategories,
+} from '../services/API';
 
 function Drinks() {
-  const { drinksList } = useContext(RecipesContext);
+  const { drinksList, setDrinksList } = useContext(RecipesContext);
+  const [drinkCategories, setDrinkCategories] = useState([]);
+  const [toggleFilter, setToggleFilter] = useState('');
+  const getDrinks = async () => {
+    const drinks = await getTwelveDrinks();
+    setDrinksList(drinks);
+    setDrinkCategories(await getDrinkCategories());
+  };
+
+  const handleClickFilter = async ({ target }) => {
+    if (toggleFilter !== target.textContent) {
+      setDrinksList(await searchByDrinkCategories(target.textContent));
+      setToggleFilter(target.textContent);
+    } else {
+      getDrinks();
+      setToggleFilter('');
+    }
+  };
+
+  useEffect(() => {
+    getDrinks();
+  }, []);
+
   return (
     <section>
       <Header title="Drinks" />
       <div>
+        {drinkCategories.map((category, index) => (
+          index <= MAX_NUMBER_BUTTON
+          && (
+            <button
+              type="button"
+              key={ category }
+              data-testid={ `${category}-category-filter` }
+              onClick={ handleClickFilter }
+            >
+              {category}
+            </button>)
+        ))}
+
+      </div>
+      <div>
         {drinksList.map((drink, index) => (index <= MAX_LIST_SIZE && <Card
-          key={ drink.idMeal }
+          key={ drink.idDrink }
           index={ index }
           image={ drink.strDrinkThumb }
           name={ drink.strDrink }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,15 +1,57 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RecipesContext from '../context/RecipesContext';
 import Card from '../components/Card';
-import { MAX_LIST_SIZE } from '../data/consts';
+import { MAX_LIST_SIZE, MAX_NUMBER_BUTTON } from '../data/consts';
+import {
+  getTwelveMeals,
+  getMealCategories,
+  searchByMealCategories,
+} from '../services/API';
 
 function Foods() {
-  const { foodsList } = useContext(RecipesContext);
+  const { foodsList, setFoodsList } = useContext(RecipesContext);
+  const [mealCategories, setMealCategories] = useState([]);
+  const [toggleFilter, setToggleFilter] = useState('');
+  const getMeals = async () => {
+    const meals = await getTwelveMeals();
+    setFoodsList(meals);
+    setMealCategories(await getMealCategories());
+  };
+
+  const handleClickFilter = async ({ target }) => {
+    if (toggleFilter !== target.textContent) {
+      setFoodsList(await searchByMealCategories(target.textContent));
+      setToggleFilter(target.textContent);
+    } else {
+      getMeals();
+      setToggleFilter('');
+    }
+  };
+
+  useEffect(() => {
+    getMeals();
+  }, []);
+
   return (
     <section>
       <Header title="Foods" />
+      <div>
+        {mealCategories.map((category, index) => (
+          index <= MAX_NUMBER_BUTTON
+          && (
+            <button
+              type="button"
+              key={ category }
+              data-testid={ `${category}-category-filter` }
+              onClick={ handleClickFilter }
+            >
+              {category}
+            </button>)
+        ))}
+
+      </div>
       <div>
         {foodsList.map((food, index) => (index <= MAX_LIST_SIZE && <Card
           key={ food.idMeal }
