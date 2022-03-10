d40ff2182c0a51be844bb3a60b5520dc4d3b7ea5
Guilherme <guilhermehallmann@gmail.com>
2022-03-10 10:56:00 -0300

    Requisito 33 quebrado

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -8,12 +8,35 @@ import favIcon from '../images/blackHeartIcon.svg';
 function Details(props) {
   const { match: { params: { id } } } = props;
   const [mealDetails, setMealDetails] = useState([]);
+  const [recipe, setRecipe] = useState([]);
   useEffect(() => {
     const request = async (mealId) => {
       const apiResult = await getMealDetails(mealId);
       setMealDetails(apiResult.meals[0]);
     };
     request(id);
+  }, []);
+  useEffect(() => {
+    const handleEntries = () => {
+      const entriesArr = Object.entries(mealDetails);
+      const newArr = entriesArr.filter((pos) => pos[0].includes('strIngredient')
+        || pos[0].includes('strMeasure'))
+        .filter((pos) => pos[1] !== '' && pos[1] !== null)
+        .map((pos) => pos[1]);
+      const SIZE = (newArr.length) / 2;
+      const firsHalf = newArr.map((pos, i) => i < SIZE && pos)
+        .filter((pos) => pos !== false);
+      const secondHalf = newArr.map((pos, i) => i >= SIZE && pos)
+        .filter((pos) => pos !== false);
+      console.log(firsHalf);
+      console.log(secondHalf);
+      for (let i = 0; i < SIZE; i += 1) {
+        recipe.push(`-${firsHalf[i]} -${secondHalf[i]}`);
+      }
+      setRecipe(recipe);
+      // console.log('teste');
+    };
+    handleEntries();
   }, []);
   return (
     <section>
@@ -32,6 +55,12 @@ function Details(props) {
           <img src={ favIcon } alt="favIcon" data-testid="favorite-btn" />
         </button>
       </section>
+      {recipe.length !== 0 && recipe.map((rec, i) => (
+        <div key={ i }>
+          <span key={ i }>{ rec }</span>
+          <span>{ }</span>
+        </div>
+      ))}
     </section>
   );
 }
