49fe0dea15d684a71e61915ad81a0ac2d9d623c3
Lucas <bithome1@outlook.com>
2022-03-11 16:26:41 -0300

    requisito 85

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
   const user = localStorage.getItem('user');
   const userObj = JSON.parse(user);
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/done-recipes');
+  };
+
   return (
     <div>
       <Header title="Profile" />
       <p data-testid="profile-email">{userObj.email}</p>
-      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ handleClick }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
         Favorite Recipes
       </button>
       <button data-testid="profile-logout-btn" type="button">Logout</button>
