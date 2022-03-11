d5bc9db2d2581fa8b48e9d130e51e676f572d743
Lucas <bithome1@outlook.com>
2022-03-11 16:05:03 -0300

    requisitos 82, 83 e 84

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,12 +3,20 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const user = localStorage.getItem('user');
+  const userObj = JSON.parse(user);
   return (
     <div>
       <Header title="Profile" />
+      <p data-testid="profile-email">{userObj.email}</p>
+      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">
+        Favorite Recipes
+      </button>
+      <button data-testid="profile-logout-btn" type="button">Logout</button>
+
       <Footer />
     </div>
   );
 }
-
 export default Profile;
